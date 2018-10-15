8d19451523a6562b10c9d5174c4855c4602c0c5e
pflickin <pflickin@microsoft.com>
2018-10-15 10:17:53 -0700

    Add healthcheck to dockerfiles.

--- Examples/base-py/my_api/runserver.py
+++ Examples/base-py/my_api/runserver.py
@@ -30,6 +30,10 @@ api_task_manager = ApiTaskManager(flask_api=api, resource_prefix=api_prefix)
 
 ai4e_wrapper = AI4EWrapper(app)
 
+@app.route('/', methods=['GET'])
+def health_check():
+    return "Health check OK"
+
 # POST, long-running/async API endpoint example
 @app.route(api_prefix + '/', methods=['POST'])
 def post():
